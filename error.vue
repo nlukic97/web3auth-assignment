d4215ac78b166bfab8f3b332ac4a21cc7871e90e
Nikola <nlukic97@gmail.com>
2023-12-06 17:46:58 +0100

    added a custom error page

---
+++ error.vue
@@ -0,0 +1,16 @@
+<script setup>
+const error = useError()
+</script>
+
+<template>
+  <div>
+    <div v-if="(error.statusCode === 404)">
+      <h1>404 - page not found</h1>
+      <span>Return to the <a href="/">Home</a> page</span>
+    </div>
+
+    <h1 v-else>Error - unable to load page.</h1>
+  </div>
+</template>
+
+<style scoped></style>
