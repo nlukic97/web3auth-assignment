87245d0814cf916d22c449862888f5cb87164615
Nikola <nlukic97@gmail.com>
2023-12-09 17:51:08 +0100

    contract calls execute

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,17 @@
 <script lang="ts" setup>
-import { ADAPTER_EVENTS, CHAIN_NAMESPACES, OPENLOGIN_NETWORK, WALLET_ADAPTERS, type CONNECTED_EVENT_DATA } from "@web3auth/base";
+// type imports
+import { ADAPTER_EVENTS, CHAIN_NAMESPACES, OPENLOGIN_NETWORK, WALLET_ADAPTERS, type CONNECTED_EVENT_DATA, type IProvider } from "@web3auth/base";
+
+// prime imports
 import { Web3AuthNoModal } from "@web3auth/no-modal";
-
 import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
-
 import { OpenloginAdapter } from "@web3auth/openlogin-adapter"
+
+// web3 import
+import Web3 from 'web3';
+
+// import smart contract abi
+import smartContractAbi from "../abi/abi.json"
 
 const chainConfig = {
   chainNamespace: CHAIN_NAMESPACES.EIP155,
@@ -35,7 +42,7 @@ const openloginAdapter = new OpenloginAdapter({
         name: "Web3Auth-Auth0-JWT",
         verifier: "web3auth-auth0",
         typeOfLogin: "jwt",
-        // clientId: clientId, //JWT client ID
+        // clientId: 'eeeaea', //JWT client ID -- TODO
       },
     },
   },
@@ -44,50 +51,66 @@ const openloginAdapter = new OpenloginAdapter({
 
 web3auth.configureAdapter(openloginAdapter);
 
-/* const subscribeAuthEvents = (web3auth: any) => {
-  web3auth.on(ADAPTER_EVENTS.CONNECTED, (data: CONNECTED_EVENT_DATA) => {
-    console.log("connected to wallet", data);
-    // web3auth.provider will be available here after user is connected
-  });
-  web3auth.on(ADAPTER_EVENTS.CONNECTING, () => {
-    console.log("connecting");
-  });
-  web3auth.on(ADAPTER_EVENTS.DISCONNECTED, () => {
-    console.log("disconnected");
-  });
-  web3auth.on(ADAPTER_EVENTS.ERRORED, (error: any) => {
-    console.log("error", error);
-  });
-  web3auth.on(ADAPTER_EVENTS.ERRORED, (error: any) => {
-    console.log("error", error);
-  });
-}; */
-
 try {
   await web3auth.init();
   console.log('init done')
 
   /* await web3auth.logout()
   console.log('logged out') */
-
   if (!web3auth.connected) {
     console.log(`Web3Auth instance is connected. Connecting...`)
 
-    const web3authProvider = await web3auth.connectTo(WALLET_ADAPTERS.OPENLOGIN, {
+    await web3auth.connectTo(WALLET_ADAPTERS.OPENLOGIN, {
       loginProvider: "apple",
     });
-    console.log('web3authprovider: ')
-    console.log(web3authProvider)
   } else {
     console.log('Already connected.')
+    // console.log('web3authprovider: ', web3authProvider)
   }
+
+  // console.log(await web3auth.authenticateUser())
+
+  console.log('Logged in. Is connected:' + web3auth.connected)
+  // console.log(await web3auth.getUserInfo());
+
+  const Iprovider = web3auth.provider
+  const web3 = new Web3(Iprovider as IProvider)
+
+  // getting connected account
+  const accounts = await web3.eth.getAccounts()
+  const userAccount = accounts[0]
+  console.log(userAccount)
+
+  // getting balance
+  console.log(await web3.eth.getBalance(userAccount))
+  console.log(await web3.eth.getBalance('0x0B3233FE361955D0Fb9796d89EeADc834c21f3FC')) // for a dev account of mine, shows correct balance
+
+  // getting PK
+  console.log(await web3.provider?.request({
+    method: 'eth_private_key'
+  }))
+
+  // smart contract instantiation
+  const smartContract = new web3.eth.Contract(smartContractAbi, '0x6168C156825d4BCD7Ccb6d25e844F661B28b8DFa')
+
+
+  // smart contract interaction - read method
+  const response = await smartContract.methods.name().call({ from: userAccount })
+  console.log('Token Name - ' + response)
   
-  console.log(await web3auth.authenticateUser())
+  // smart contract interaction - read method - balance of tokens
+  const response2 = await smartContract.methods.balanceOf(userAccount).call({ from: userAccount })
+  console.log(response2)
 
-  console.log('Loged in. Is connected:' + web3auth.connected)
-  console.log(await web3auth.getUserInfo());
+  // smart contract interaction - write method - mint yourself 1 token
+  /* await smartContract.methods.mint(userAccount, '1000000000000000000').send({ from: userAccount })
+    .on('receipt', (data) => console.log(data))
+    .on('error', (err) => console.log(err)) */
 
-  const Iprovider = web3auth.provider // should I continue from here?
+
+
+
+
 } catch (e) {
   console.error(e)
 }
